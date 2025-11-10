2a13ecb2441715c0841b4553dab07e73d9b2259b
Kirill <ignatovkirill5@gmail.com>
2025-11-10 22:30:37 +0300

    9. Получение одной категории. Страница категории

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -8,7 +8,7 @@
         <!--begin::Row-->
         <div class="row">
           <div class="col-sm-6">
-            <h3 class="mb-0">Dashboard</h3>
+            <h3 class="mb-0">{{ $category->title }}</h3>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-end">
@@ -29,37 +29,27 @@
         <!--begin::Row-->
         <div class="row">
           <!--begin::Col-->
-          <div class="col-1">
-            <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Добавить</a>
-          </div>
-          </div>
-          <div class="row">
+        <div class="row">
           <div class="col-12">
             <table class="table mt-3 w-25">
-              <thead>
-                <tr>
-                  <th scope="col">ID</th>
-                  <th scope="col">Название</th>
-                  <th scope="col">Действия</th>
-                </tr>
-              </thead>
               <tbody>
-                @foreach ($categories as $category)
                 <tr>
+                  <td>ID</td>
                   <td>{{ $category->id }}</td>
-                  <td>{{ $category->title }}</td>
-                  <td><a href="{{ route('admin.category.show', $category->id) }}"><i class="bi bi-eye"></i></a></td>
                 </tr>
-                @endforeach
+                <tr>
+                  <td>Название</td>
+                  <td>{{ $category->title }}</td>
+                </tr>
               </tbody>
             </table>
           </div>
-          </div>
-          <!--begin::Small Box Widget 4-->
         </div>
-        <!--end::Small Box Widget 4-->
+        <!--begin::Small Box Widget 4-->
       </div>
-      <!--end::Col-->
+      <!--end::Small Box Widget 4-->
+    </div>
+    <!--end::Col-->
     </div>
     <!--end::Row-->
     <!--begin::Row-->
