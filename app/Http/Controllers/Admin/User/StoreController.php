f91b8ed0b8675dd89590d4e86290450d031aabd5
Kirill <ignatovkirill5@gmail.com>
2025-11-24 20:46:25 +0300

    28. Отправление письма с подтверждением учетной записи Email verification

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\Admin\User\StoreRequest;
 use App\Http\Controllers\Controller;
 use App\Mail\User\PasswordMail;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
@@ -15,15 +16,11 @@ class StoreController extends Controller
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-
         $password = Str::random(10);
-
         $data['password'] = Hash::make($password);
-
-        User::firstOrCreate(['email' => $data['email']], $data);
-
+        $user = User::firstOrCreate(['email' => $data['email']], $data);
         Mail::to($data['email'])->send(new PasswordMail($password));
-
+        event(new Registered($user));
         return redirect()->route('admin.user.index');
     }
 }
